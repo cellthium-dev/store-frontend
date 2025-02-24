3dc683c87907af428fb517dd81dee9b65e44ee40
Felix Arjuna <felixarjuna.de@yahoo.com>
2025-02-24 14:04:46 +0100

    feature: add google login button

--- src/modules/account/components/login/index.tsx
+++ src/modules/account/components/login/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/_components/ui/button"
+import { Separator } from "@/_components/ui/separator"
 import { login } from "@lib/data/customer"
 import { toast } from "@medusajs/ui"
 import { LOGIN_VIEW } from "@modules/account/templates/login-template"
@@ -6,6 +8,7 @@ import { SubmitButton } from "@modules/checkout/components/submit-button"
 import Input from "@modules/common/components/input"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import React from "react"
+import { FaGoogle } from "react-icons/fa"
 
 type Props = {
   setCurrentView: (view: LOGIN_VIEW) => void
@@ -29,35 +32,50 @@ const Login = ({ setCurrentView }: Props) => {
       <p className="text-center text-base-regular text-ui-fg-base mb-8">
         Sign in to access an enhanced shopping experience.
       </p>
-      <form className="w-full" action={formAction}>
-        <div className="flex flex-col w-full gap-y-2">
-          <Input
-            label="Email"
-            name="email"
-            type="email"
-            title="Enter a valid email address."
-            autoComplete="email"
-            required
-            data-testid="email-input"
+
+      <div className="w-full grid gap-y-2">
+        <form className="w-full " action={formAction}>
+          <div className="flex flex-col w-full gap-y-2">
+            <Input
+              label="Email"
+              name="email"
+              type="email"
+              title="Enter a valid email address."
+              autoComplete="email"
+              required
+              data-testid="email-input"
+            />
+            <Input
+              label="Password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              data-testid="password-input"
+            />
+          </div>
+          <ErrorMessage
+            error={response?.message}
+            data-testid="login-error-message"
           />
-          <Input
-            label="Password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            data-testid="password-input"
-          />
+          <SubmitButton data-testid="sign-in-button" className="w-full mt-6">
+            Sign in
+          </SubmitButton>
+        </form>
+
+        <div className="flex items-center w-full gap-x-4 ">
+          <Separator orientation="horizontal" className="flex-1" />
+          <span className="mx-auto">or</span>
+          <Separator orientation="horizontal" className="flex-1" />
         </div>
-        <ErrorMessage
-          error={response?.message}
-          data-testid="login-error-message"
-        />
-        <SubmitButton data-testid="sign-in-button" className="w-full mt-6">
-          Sign in
-        </SubmitButton>
-      </form>
-      <span className="text-center text-ui-fg-base text-small-regular mt-6">
+
+        <Button variant={"outline"}>
+          <FaGoogle className="mr-2" />
+          Continue with Google
+        </Button>
+      </div>
+
+      <span className="text-center text-ui-fg-base text-small-regular mt-4">
         Not a member?{" "}
         <button
           onClick={() => setCurrentView(LOGIN_VIEW.REGISTER)}
@@ -68,7 +86,7 @@ const Login = ({ setCurrentView }: Props) => {
         </button>
         .
       </span>
-      <span className="text-center text-ui-fg-base text-small-regular mt-6">
+      <span className="text-center text-ui-fg-base text-small-regular mt-4">
         Forgot your password?{" "}
         <LocalizedClientLink
           href="/account/reset-password"
